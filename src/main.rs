0753592fffdcb0464382f6e742a861f004e7e8db
Eusef Karl Blancada <eoblancada@up.edu.ph>
2026-08-26 21:56:35 +0800

    feat: rudimentary scanner implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env, fs, process};
+
+
+fn fail(message: &str) -> ! {
+    eprintln!("lab 0 error: {}", message);
+    process::exit(65);
+}
+
+
 fn main(){
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+
+    if args.len() != 2 {
+        fail("Usage: lab0 <source-file>");
+    }
+
+    let filepath = &args[1];
+
+    let contents = fs::read_to_string(path).
+    unwrap_or_else(|error| fail("Failed to read file: {}"));
+
+    print!("{contents}");
 }
